f9a94551098768e669ff364b7d27ae31ac0b506a
Daniele Tricoli <eriol@mornie.org>
2021-07-02 20:40:48 +0200

    Add decode test tests

--- src/eudcc.rs
+++ src/eudcc.rs
@@ -38,6 +38,23 @@ struct RecoveryRecord {
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
+struct TestRecord {
+    tg: String,
+    tt: String,
+    #[serde(default)]
+    nm: String,
+    ma: String,
+    sc: String,
+    #[serde(default)]
+    dr: String,
+    tr: String,
+    tc: String,
+    co: String,
+    is: String,
+    ci: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
 struct Name {
     #[serde(rename = "fn")]
     fn_: String,
@@ -55,6 +72,8 @@ pub struct Certificate {
     v: Vec<VaccineRecord>,
     #[serde(default)]
     r: Vec<RecoveryRecord>,
+    #[serde(default)]
+    t: Vec<TestRecord>,
 }
 
 pub fn decode(data: String) -> Result<Certificate> {
@@ -123,10 +142,11 @@ fn decode_vaccination_test() {
             ci: "01ITE7300E1AB2A84C719004F103DCB1F70A#6".to_string(),
         }],
         r: vec![],
+        t: vec![],
     };
 
-    let c1 = decode(vaccination_data.to_string()).unwrap();
-    assert_eq!(c1, expected);
+    let c = decode(vaccination_data.to_string()).unwrap();
+    assert_eq!(c, expected);
 }
 
 #[test]
@@ -154,8 +174,78 @@ fn decode_recovery_test() {
             du: "2021-10-31".to_string(),
             ci: "01ITA65E2BD36C9E4900B0273D2E7C92EEB9#1".to_string(),
         }],
+        t: vec![],
     };
 
-    let c2 = decode(recovery_data.to_string()).unwrap();
-    assert_eq!(c2, expected);
+    let c = decode(recovery_data.to_string()).unwrap();
+    assert_eq!(c, expected);
+}
+
+#[test]
+fn decode_test_test() {
+    // Taken from:
+    // https://github.com/eu-digital-green-certificates/dgc-testdata/blob/main/IT/2DCode/raw/3.json
+    // It is licensed under Apache-2.0 License.
+    let test_data = "HC1:6BFOXN%TS3DH0YOJ58S S-W5HDC *M0IIE 1C9B5G2+$NP-OP-IA%N%QHRJPC%OQHIZC4.OI:OIG/Q80P2W4VZ0K1H$$0CNN62PK.G +AG5T01HJCAMKNAB5S.8%*8Z95%9EMP8N22MM42WFCD9C2AKIJKIJM1MQIAY.D-7A4KE0PLV1ARKF.GH5$C4-9GGIUEC0QE1JAF.714NTPINRQ3.VR+P0$J2*N$*SB-G9+RT*QFNI2X02%KYZPQV6YP8412HOA-I0+M9GPEGPEMH0SJ4OM9*1B+M96K1HK2YJ2PI0P:65:41ZSW$P*CM-NT0 2$88L/II 05B9.Z8T*8Y1VM:KCY07LPMIH-O9XZQ4H9IZBP%D2U3+KGP2W2UQNG6-E6+WJTK1%J6/UI2YUELE+W35T7+H8NH8DRG+PG.UIZ$U%UF*QHOOENBU621TW5XW5HS9+I010H% 0R%0ZD5CC9T0HP8TCNNI:CQ:G172DX8FZV3U9W-HNPPQ N2KV 2VHDHO:2XAV:FB+18DRR%%VQ F60LF6K 38GK8LGG4U7UP6*S4QBR-F97FRONPKZS+P9$5W1CAV37KD48ERCRH";
+    let expected = Certificate {
+        ver: "1.0.0".to_string(),
+        nam: Name {
+            fn_: "Di Caprio".to_string(),
+            fnt: "DI<CAPRIO".to_string(),
+            gn: "Marilù Teresa".to_string(),
+            gnt: "MARILU<TERESA".to_string(),
+        },
+        dob: "1977-06-16".to_string(),
+        v: vec![],
+        r: vec![],
+        t: vec![TestRecord {
+            tg: "840539006".to_string(),
+            tt: "LP6464-4".to_string(),
+            nm: "Roche LightCycler qPCR".to_string(),
+            ma: "1232".to_string(),
+            sc: "2021-05-03T10:27:15Z".to_string(),
+            dr: "2021-05-11T12:27:15Z".to_string(),
+            tr: "260415000".to_string(),
+            tc: "Policlinico Umberto I".to_string(),
+            co: "IT".to_string(),
+            is: "IT".to_string(),
+            ci: "01IT053059F7676042D9BEE9F874C4901F9B#3".to_string(),
+        }],
+    };
+
+    let c = decode(test_data.to_string()).unwrap();
+    assert_eq!(c, expected);
+
+    // Taken from:
+    // https://github.com/eu-digital-green-certificates/dgc-testdata/blob/main/IT/2DCode/raw/4.json
+    // It is licensed under Apache-2.0 License.
+    let test_data = "HC1:6BFOXN%TS3DH0YOJ58S S-W5HDC *M0II*%6C9B5G2+$NEJPP-IA%NGRIRJPC%OQHIZC4.OI:OIG/Q80P2W4VZ0K1H$$05QN*Y0K.G +AG5T01HJCAMKN$71Z95Z11VTO.L8YBJ-B93:GQBGZHHBIH5C99.B4DBF:F0.8ELG:.CC-8LQECKEBLDSH8XAG.6A-JE:GQA KX-SZDG0$JO+SW*PR+PHXF8IQV$K%OKOUFBBQR-S3D1PI0/7Q.H0807-L9CL62/2JJ11K2919GI1X1DDM8RMA0/41:6Z.2:NC-%CN$KJLCLF9+FJE 4Y3LL/II 05B9.Z8M+8:Y001HCY0R%0IGF5JNCPIGSUNG6YS75XJ/J0/V7.UI$RU8ZB.W2FI28LHUZUYZQNI9Y FQQGQ$FP DDVBDVBBX33UQLTU8L20H6/*12SADB9:G9J+9Y 5LJA8JF8JFHJP7NVDEBK3JQ7TI 05QNT+CCZ1ZA2I+T*R9XZ6/:COTJCURIF8CZPCJ4EF5LU5I-Q:.N$P9DX5NAM*PJYD3L2V0GBG.JL4LESU72S1CM%5OC%VSTJ8NC1TGO:QS02V505GJUTH";
+    let expected = Certificate {
+        ver: "1.0.0".to_string(),
+        nam: Name {
+            fn_: "Di Caprio".to_string(),
+            fnt: "DI<CAPRIO".to_string(),
+            gn: "Marilù Teresa".to_string(),
+            gnt: "MARILU<TERESA".to_string(),
+        },
+        dob: "1977-06-16".to_string(),
+        v: vec![],
+        r: vec![],
+        t: vec![TestRecord {
+            tg: "840539006".to_string(),
+            tt: "LP6464-4".to_string(),
+            nm: "Roche LightCycler qPCR".to_string(),
+            ma: "".to_string(),
+            sc: "2021-05-10T10:27:15Z".to_string(),
+            dr: "2021-05-11T12:27:15Z".to_string(),
+            tr: "260415000".to_string(),
+            tc: "Policlinico Umberto I".to_string(),
+            co: "IT".to_string(),
+            is: "IT".to_string(),
+            ci: "01IT0BFC9866D3854EAC82C21654B6F6DE32#1".to_string(),
+        }],
+    };
+
+    let c = decode(test_data.to_string()).unwrap();
+    assert_eq!(c, expected);
 }
